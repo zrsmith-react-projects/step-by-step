e773e46bb9eb575edee26e1005e3536fa9567c5a
Zac Smith <zrs3141592@gmail.com>
2019-08-19 22:20:16 -0600

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
+import "./App.css";
+
+const Parent = props => {
+  return <div>Parent: {props.personData.parentName}</div>;
+};
+
+const App = () => {
+  const personData = {
+    parentName: "Randy",
+    childName: "Seth",
+    grandchildName: "Tara"
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Parent personData={personData} />
     </div>
   );
-}
+};
 
 export default App;
